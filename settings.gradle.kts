67ccc47ec35d2d45f7bafc9e26fc10a1be311115
Flyman <icediv@163.com>
2022-08-01 13:53:22 +0800

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,24 @@
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+        google()
+        mavenCentral()
+    }
+}
+
+dependencyResolutionManagement {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = ("kotlin-android-template")
+
+enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
+
+include(
+    "app",
+    "library-android",
+    "library-kotlin"
+)
